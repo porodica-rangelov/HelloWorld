9bf111fbf82b4b557507315b22ecf8d50d6954e4
Rangelov Djole <rangelov.djole@gmail.com>
2026-02-04 15:20:13 +0100

    zadatak1.py added

---
+++ zadatak1.py
@@ -0,0 +1,11 @@
+#Zadatak 1)
+print("Dobrodosli u Suma cifara visecifrenog broja")
+broj = int(input("Unesi visecifreni broj: "))
+suma = 0
+
+while broj > 0:
+    cifra = broj % 10
+    suma += cifra
+    broj //= 10
+
+print("Suma cifara je:", suma)
